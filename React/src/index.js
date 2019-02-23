970992c4d5fae7bb68d647c7ad133c7356eb4359
Gayan Menikpura <gayan.m@eyepax.com>
2019-02-24 00:20:59 +0530

    Global loader compnent with dynamic funciton

--- React/src/index.js
+++ React/src/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import "./app.css";
 
-function App() {
-  return (
-    <div class="page">
-    </div>
-  );
+import RantList from "./RantList";
+import Header from "./Header";
+import Loader from "./Loader";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        <Header />
+        <section className="main layout--center">
+          <div className="main__content layout--wrapped">
+            <Loader 
+              isLoading={true}
+            />
+            <RantList />
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <App />
-, rootElement);
+const rootElement = document.getElementById("äpp");
+ReactDOM.render(<App />, rootElement);
