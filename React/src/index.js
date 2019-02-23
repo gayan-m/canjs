7b978b5b039563122a14b1825a720958ea229839
Gayan Menikpura <gayan.m@eyepax.com>
2019-02-24 01:17:37 +0530

    implement routs for rant_list & rant_details pages

--- React/src/index.js
+++ React/src/index.js
@@ -1,29 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./app.css";
 
-import RantList from "./RantList";
 import Header from "./Header";
 import Loader from "./Loader";
+import RantList from "./RantList";
+import RantDetails from "./RantDetails";
 
 class App extends React.Component {
   render() {
     return (
+      <Router>
       <div className="page">
         <Header />
         <section className="main layout--center">
           <div className="main__content layout--wrapped">
             <Loader 
-              isLoading={true}
+              isLoading={false}
             />
-            <RantList />
+            <Route exact path="/" component={RantList} />
+            <Route exact path="/rant/:rant_id" component={RantDetails} />
           </div>
         </section>
       </div>
+      </Router>
     );
   }
 }
 
 const rootElement = document.getElementById("äpp");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<App />
+, rootElement);
